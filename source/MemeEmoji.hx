5eb8b0c681510599fad642a84a0f20d1146afd29
gr8-idea-m8 <intl.chickenfist@gmail.com>
2015-08-26 14:44:17 -0700

    sync frame rate

--- source/MemeEmoji.hx
+++ source/MemeEmoji.hx
@@ -37,7 +37,7 @@ class MemeEmoji extends FlxSprite
         {
                 super(X * 64 + 16, Y * 64 + 16);
                 loadGraphic("assets/images/memeface2.png", true, 32, 32);
-                animation.add("idle1", [1, 2, 3], 36, true);
+                animation.add("idle1", [1, 2, 3], 1, true);
                 animation.play("idle1");
                 drag.x = drag.y = 1600;
         }
